928b4eb2ef9e9e0a5f153719c8d5b9f81d377e1c
AktresS <115887485+AktresS@users.noreply.github.com>
2025-09-22 13:41:01 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryControllerApi;
 use App\Http\Controllers\DishControllerApi;
 use App\Http\Controllers\IngredientControllerApi;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dish', [DishControllerApi::class, 'index']);
+Route::post('login', [AuthController::class, 'login']);
+
+//Route::middleware(['auth:sanctum'])->get('/user', function (Request $request){
+//    return $request->user();
+//});
+//
+//Route::middleware(['auth:sanctum'])->get('/logout', [AuthController::class, 'logout']);
+//
+//Route::middleware('auth:sanctum')->get('/dish', [DishControllerApi::class, 'index']);
 Route::get('/dish/{id}', [DishControllerApi::class, 'show']);
 
 Route::get('/category', [CategoryControllerApi::class, 'index']);
@@ -13,3 +23,11 @@ Route::get('/category/{id}', [CategoryControllerApi::class, 'show']);
 
 Route::get('/ingredient', [IngredientControllerApi::class, 'index']);
 Route::get('/ingredient/{id}', [IngredientControllerApi::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/dish', [DishControllerApi::class, 'index']);
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
