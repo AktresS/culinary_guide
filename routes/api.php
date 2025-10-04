147c8356b38c7fd3afc4acb7315dc8ca6484b792
AktresS <115887485+AktresS@users.noreply.github.com>
2025-10-04 19:24:28 +0500

    Getting data from tables

--- routes/api.php
+++ routes/api.php
@@ -9,23 +9,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 
-//Route::middleware(['auth:sanctum'])->get('/user', function (Request $request){
-//    return $request->user();
-//});
-//
-//Route::middleware(['auth:sanctum'])->get('/logout', [AuthController::class, 'logout']);
-//
-//Route::middleware('auth:sanctum')->get('/dish', [DishControllerApi::class, 'index']);
 Route::get('/dish/{id}', [DishControllerApi::class, 'show']);
+Route::get('/dishes', [DishControllerApi::class, 'index']);
+Route::get('/dishes_total', [DishControllerApi::class, 'total']);
 
 Route::get('/category', [CategoryControllerApi::class, 'index']);
 Route::get('/category/{id}', [CategoryControllerApi::class, 'show']);
+Route::get('/categories_total', [CategoryControllerApi::class, 'total']);
 
 Route::get('/ingredient', [IngredientControllerApi::class, 'index']);
 Route::get('/ingredient/{id}', [IngredientControllerApi::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/dish', [DishControllerApi::class, 'index']);
     Route::get('/user', function (Request $request){
         return $request->user();
     });
